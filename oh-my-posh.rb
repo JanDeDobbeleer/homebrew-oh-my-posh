3cee47b01231652f4153000a7738ecafb5e94b39
JanDeDobbeleer <JanDeDobbeleer@users.noreply.github.com>
2020-11-10 17:43:08 +0000

    feat: Formula v3.30.2

--- oh-my-posh.rb
+++ oh-my-posh.rb
@@ -1,10 +1,10 @@
 class OhMyPosh < Formula
   desc "Prompt theme engine for any shell"
   homepage "https://ohmyposh.dev"
-  url "https://github.com/JanDeDobbeleer/oh-my-posh3/archive/v3.30.1.tar.gz"
-  sha256 "a68fa9e410a7643c360c8bdd59eb53d0598b5c011adfc47646ea9037483367b0"
+  url "https://github.com/JanDeDobbeleer/oh-my-posh3/archive/v3.30.2.tar.gz"
+  sha256 "ef424c74ea29e873810240d8cbb084c57a9778ff059260f718f4aab49a25e3b4"
   license "GPL-3.0-only"
-  version "3.30.1"
+  version "3.30.2"
 
   depends_on "go" => :build
 
