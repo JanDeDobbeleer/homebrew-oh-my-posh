c47a4358552fec3ae3de2ce3bd7765e2bfcc3119
Jan De Dobbeleer <jan.de.dobbeleer@gmail.com>
2020-12-23 22:49:11 +0100

    fix: working version 3.60.0

--- oh-my-posh.rb
+++ oh-my-posh.rb
@@ -5,19 +5,24 @@ class OhMyPosh < Formula
   sha256 "971a090dc82708dd6abc379c05d4cac20bc132aaacee9bc710895b03997e9f57"
   license "GPL-3.0-only"
   version "3.60.0"
-  revision 1
+  revision 2
 
   depends_on "go" => :build
+  depends_on "go-bindata" => :build
 
   def install
-    system "go", "get", "-u", "github.com/kevinburke/go-bindata/..."
     system "go-bindata", "-o", "init.go", "init/"
-    system "go", "build", "-o", "oh-my-posh"
+    system "go", "build", "-o", "oh-my-posh", "-ldflags=\"-X 'main.Version=3.60.0'\""
     bin.install "oh-my-posh"
     mv "themes", prefix
-    ohai "Thanks for installing Oh my Posh."
-    ohai "Have a look at https://ohmyposh.dev/docs/installation for detailed instructions for your shell."
-    ohai "Sample themes can be found at $(brew --prefix oh-my-posh)/themes."
+  end
+
+  def caveats
+    <<~EOS
+      Thanks for installing Oh my Posh.
+      Have a look at https://ohmyposh.dev/docs/installation for detailed instructions for your shell.
+      Sample themes can be found at $(brew --prefix oh-my-posh)/themes.
+    EOS
   end
 
   test do
