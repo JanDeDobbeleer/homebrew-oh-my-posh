75dfdb07cb91c6fceb5877a35af295ab90c2a846
JanDeDobbeleer <JanDeDobbeleer@users.noreply.github.com>
2021-06-16 20:33:15 +0000

    feat: Formula v3.163.1

--- oh-my-posh.rb
+++ oh-my-posh.rb
@@ -1,16 +1,16 @@
 class OhMyPosh < Formula
   desc "Prompt theme engine for any shell"
   homepage "https://ohmyposh.dev"
-  url "https://github.com/JanDeDobbeleer/oh-my-posh/archive/v3.163.0.tar.gz"
-  sha256 "64b841bec43e8a112ed9cf06fd79700ffa561ee1afba913c6b381725dbfc9d53"
+  url "https://github.com/JanDeDobbeleer/oh-my-posh/archive/v3.163.1.tar.gz"
+  sha256 "8dad135ffaa1cfe817797e29cf48f9621be208f8708760ce5ffb0cd4cc14af4e"
   license "GPL-3.0-only"
-  version "3.163.0"
+  version "3.163.1"
 
   depends_on "go@1.16" => :build
 
   def install
     Dir.chdir("src") do
-      system("go build -o=oh-my-posh -ldflags=\"-X \'main.Version=3.163.0\'\"")
+      system("go build -o=oh-my-posh -ldflags=\"-X \'main.Version=3.163.1\'\"")
       bin.install "oh-my-posh"
     end
     mv "themes", prefix
