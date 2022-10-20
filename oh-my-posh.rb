b2ab53c9df865486ad1117ca4d140ebb8b36afb7
JanDeDobbeleer <JanDeDobbeleer@users.noreply.github.com>
2022-10-20 05:05:34 +0000

    feat: Formula v12.6.4

--- oh-my-posh.rb
+++ oh-my-posh.rb
@@ -1,18 +1,18 @@
 class OhMyPosh < Formula
   desc "Prompt theme engine for any shell"
   homepage "https://ohmyposh.dev"
-  url "https://github.com/JanDeDobbeleer/oh-my-posh/archive/v12.6.3.tar.gz"
+  url "https://github.com/JanDeDobbeleer/oh-my-posh/archive/v12.6.4.tar.gz"
   head "https://github.com/JanDeDobbeleer/oh-my-posh.git", branch: "main"
-  sha256 "a78aee1cc9fb7ca475cd6303294add086ee6c78429101c36bc9559fa8a54893f"
+  sha256 "bf8c44efb4244a323019aca18811d5ad175af554ec3b281ad87e145af650ef6e"
   license "MIT"
-  version "12.6.3"
+  version "12.6.4"
 
   depends_on "go@1.19" => :build
 
   def install
     Dir.chdir("src") do
       ENV["GOPROXY"] = ENV.has_key?("HOMEBREW_GOPROXY") ? ENV["HOMEBREW_GOPROXY"] : ""
-      system("go build -o=oh-my-posh -ldflags=\"-s -w -X \'main.Version=12.6.3\'\"")
+      system("go build -o=oh-my-posh -ldflags=\"-s -w -X \'main.Version=12.6.4\'\"")
       bin.install "oh-my-posh"
     end
     mv "themes", prefix
