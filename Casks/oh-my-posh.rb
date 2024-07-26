4f1ecef66835ac5cd127e74eda77b676afc89bf7
JanDeDobbeleer <JanDeDobbeleer@users.noreply.github.com>
2024-07-26 06:21:39 +0000

    feat: Formula v23.0.0

--- Casks/oh-my-posh.rb
+++ Casks/oh-my-posh.rb
@@ -2,11 +2,11 @@ cask "oh-my-posh" do
     desc "Prompt theme engine for any shell"
     homepage "https://ohmyposh.dev"
     arch arm: "arm64", intel: "amd64"
-    version "22.3.0"
+    version "23.0.0"
     url "https://github.com/JanDeDobbeleer/oh-my-posh/releases/download/v#{version}/posh-darwin-#{arch}",
         verified: "github.com/JanDeDobbeleer/oh-my-posh/"
-    sha256 arm:   "0524315a7b46296085000190696da25e284b0839bd064700c7fa02f5e4ae25fa",
-           intel: "292959ec297605c8c5d765556088ac728f914b76129c67e64626d10f0bd9205d"
+    sha256 arm:   "171df19648e32ded7f51897d42b28e1c60c28d7cd041aa57438db2f5af42a177",
+           intel: "09c2762abf330c6e8ea3f201054891573d228c32918d42091b3296257d9b20c7"
     name "oh-my-posh"
     binary "posh-darwin-#{arch}", target: "oh-my-posh"
     auto_updates true
