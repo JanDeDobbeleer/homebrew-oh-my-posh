d6ec006126ca18ca91127488dc98ed7acb770382
JanDeDobbeleer <JanDeDobbeleer@users.noreply.github.com>
2024-08-29 09:15:41 +0000

    feat: Formula v23.9.0

--- Casks/oh-my-posh.rb
+++ Casks/oh-my-posh.rb
@@ -2,11 +2,11 @@ cask "oh-my-posh" do
     desc "Prompt theme engine for any shell"
     homepage "https://ohmyposh.dev"
     arch arm: "arm64", intel: "amd64"
-    version "23.8.0"
+    version "23.9.0"
     url "https://github.com/JanDeDobbeleer/oh-my-posh/releases/download/v#{version}/posh-darwin-#{arch}",
         verified: "github.com/JanDeDobbeleer/oh-my-posh/"
-    sha256 arm:   "15c6438490d86eed79a32a69a2b6f7d7855b6f788936510ad5bdbb17425f9aa2",
-           intel: "fc56c86e57e4de781b790d5df99041c05f5e84556d20beb6f9ed8ae0765ff16e"
+    sha256 arm:   "242c790291799b6387999edc3e5402b09465a963b5a7fbccbcb06cc1f9838505",
+           intel: "0b31fcc510b3ad27d43e2f5f9830b46801d2c87a3416f44a2f2ef43c231cfcd8"
     name "oh-my-posh"
     binary "posh-darwin-#{arch}", target: "oh-my-posh"
     auto_updates true
--- oh-my-posh.rb
+++ oh-my-posh.rb
@@ -1,18 +1,18 @@
 class OhMyPosh < Formula
   desc "Prompt theme engine for any shell"
   homepage "https://ohmyposh.dev"
-  url "https://github.com/JanDeDobbeleer/oh-my-posh/archive/v23.8.0.tar.gz"
+  url "https://github.com/JanDeDobbeleer/oh-my-posh/archive/v23.9.0.tar.gz"
   head "https://github.com/JanDeDobbeleer/oh-my-posh.git", branch: "main"
-  sha256 "ddcdbdbbf65c51fda36fe1f24a19658f53915769b7df0ea483909eecfa1e3562"
+  sha256 "8a2a5cb4913acfcc7a42c970449282dca167893fd99a29c289093c399aec1477"
   license "MIT"
-  version "23.8.0"
+  version "23.9.0"
 
   depends_on "go@1.22" => :build
 
   def install
     Dir.chdir("src") do
       ENV["GOPROXY"] = ENV.has_key?("HOMEBREW_GOPROXY") ? ENV["HOMEBREW_GOPROXY"] : ""
-      system("go build -o=oh-my-posh -ldflags=\"-s -w -X \'github.com/jandedobbeleer/oh-my-posh/src/build.Version=23.8.0\' -X \'github.com/jandedobbeleer/oh-my-posh/src/build.Date=2024-08-27T07:51:00Z\'\"")
+      system("go build -o=oh-my-posh -ldflags=\"-s -w -X \'github.com/jandedobbeleer/oh-my-posh/src/build.Version=23.9.0\' -X \'github.com/jandedobbeleer/oh-my-posh/src/build.Date=2024-08-29T09:15:39Z\'\"")
       bin.install "oh-my-posh"
     end
     mv "themes", prefix
